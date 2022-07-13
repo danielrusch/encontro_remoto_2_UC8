ac99cdeed8a278c598c811d1a39de2d516bc2f27
Daniel Rusch <daniel-rusch@hotmail.com>
2022-07-13 20:33:01 -0300

    Atividade executada

---
+++ listaDeAlunos.js
@@ -0,0 +1,40 @@
+
+let listaDeAlunos = ["Flavio","Gustavo","Nicolas","Mauricio"]
+
+for (let nome of listaDeAlunos) {
+    console.log("O nome do aluno é " + nome + " no EAD Senai")
+}
+
+// TESTES EM AULA  -------------------------------------------------------------------
+
+
+// for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+//     console.log(contador); 
+
+    // if (contador == 0) {
+    // console.log("numero igual a Zero")
+    //  } 
+    // else if (contador % 2 == 0) {
+    //     console.log("número par")
+    // } 
+    // else {
+    //     console.log("número impar")
+    // }
+// }
+
+// -----------------------------------------------
+
+// let contador = 0
+
+// while(contador < numeroDeAlunos) {
+//     if (contador == 0) {
+//         console.log("numero igual a Zero")
+//          } 
+//         else if (contador % 2 == 0) {
+//             console.log("número par")
+//         } 
+//         else {
+//             console.log("número impar")
+//         }
+//         contador++
+// }
